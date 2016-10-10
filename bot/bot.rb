9dc33abb4ab803c0ebf57e4aaa30bd3a9e60e143
University of Bath Digital Team <web-support@bath.ac.uk>
2016-10-10 22:34:49 +0100

    Update bot.rb

--- bot/bot.rb
+++ bot/bot.rb
@@ -5,12 +5,13 @@ class Bot < SlackRubyBot::Bot
     @id = @id % 10 + 1
   end
 
-  command 'say' do |client, data, match|
-    Rails.cache.write next_id, { text: match['expression'] }
-    client.say(channel: data.channel, text: match['expression'])
-  end
+  #command 'say' do |client, data, match|
+  #  Rails.cache.write next_id, { text: match['expression'] }
+  #  client.say(channel: data.channel, text: match['expression'])
+  #end
   
   match /^How is the weather in (?<location>\w*)\?$/ do |client, data, match|
+    Rails.cache.write next_id, { text: match['location'] }
     client.say(channel: data.channel, text: "The weather in #{match[:location]} is nice.")
   end
   
