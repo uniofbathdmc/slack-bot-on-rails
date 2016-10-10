78e4a5bf9770beeca10d7d993bd78951a80229ed
University of Bath Digital Team <web-support@bath.ac.uk>
2016-10-10 22:28:00 +0100

    Update bot.rb

--- bot/bot.rb
+++ bot/bot.rb
@@ -9,4 +9,8 @@ class Bot < SlackRubyBot::Bot
     Rails.cache.write next_id, { text: match['expression'] }
     client.say(channel: data.channel, text: match['expression'])
   end
+  
+  match(/^(.*)$/i) do |client, data, match|
+    client.say(channel: data.channel, text: "I'm going to post that to the internet! (#{match[:location]})")
+  end
 end
