e8ee529db74462e2eecd40689c49b99aff95dbad
University of Bath Digital Team <web-support@bath.ac.uk>
2016-10-14 15:40:00 +0100

    Update messages_controller.rb

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,5 +1,10 @@
 class MessagesController < ApplicationController
+  
   def index
+    data = { id: 1, message: Rails.cache.read(1) }
+  end
+  
+  def index_ten
     data = (1..10).map do |i|
       { id: i, message: Rails.cache.read(i) }
     end
